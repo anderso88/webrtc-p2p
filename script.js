cc7e94f4550e1c4bbcf6e5fd1b104bb3965f737d
anderso88 <146121136+anderso88@users.noreply.github.com>
2025-02-03 00:21:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbz0-Z53mKBmPAu1jYXU9lQGSqAIcv_zhEKXl17xrGGvAMoZl4XpHtrbI9ZJs6zohvhVew/exec"; // Cole a URL gerada pelo Apps Script
-
+const API_URL = https://script.google.com/macros/s/AKfycbyEgxI7N8dRjWnEWYD1OSUQ7fn-OLfyaZK91LzoLeynOtT3CIINKEYjmhbCdjYsP2mGcg/exec
 async function fazerLogin() {
     let usuario = document.getElementById("usuario").value.trim();
     let senha = document.getElementById("senha").value.trim();
